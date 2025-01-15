734a86c6f16864df7dc8ca8960aa4aab0611493c
azim <azimirfan25@gmail.com>
2025-01-15 16:29:16 +0800

    Clear screen function

--- Hangman.py
+++ Hangman.py
@@ -8,6 +8,13 @@ GREEN = "\033[32m"
 RESET = "\033[0m"
 BOLD = "\033[1m"
 
+# Clear screen function for a clean display
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 # ASCII Art for the Hangman Stages with green color and border
 hangman_stages = [
     f"""{GREEN}
