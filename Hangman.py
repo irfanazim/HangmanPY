4a163e2e0bde2d8ecd7237cfaaf80def1a8454e4
azim <azimirfan25@gmail.com>
2025-01-16 22:55:13 +0800

    Header Comment

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,13 @@
+# *************************************************************************
+# Program: Hangman.py
+# Course: CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN
+# Lecture / Lab Section: TC2L / TL6L
+# Trimester: 2430
+# Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3 | MEMBER_NAME_4
+# IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 | MEMBER_ID_3
+# Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 | MEMBER_EMAIL_3
+# *************************************************************************
+
 import json
 import random
 import os
